2b1906266d3d735bc16c28e87ee88429f76aac28
Red Pi <plerouge@eleven-labs.com>
2019-09-27 14:37:00 +0200

    manage date file

--- file.js
+++ file.js
@@ -1,17 +1,19 @@
 const { appendFile, mkdir } = require('fs');
 const { join } = require('path');
 const { connection } = require('./database.js');
-
-const destDir = 'monDossier';
+const { format } = require('date-fns');
+const destDir = 'movies';
 
 const createMoviesFile = () => {
   return new Promise((resolve, reject) => {
     connection.query(`SELECT * FROM movies`, (error, results) => {
       if (error) reject();
       mkdir(destDir, () => {
-        const filenameToCreate = join(destDir, `movies.txt`);
+        const date = format(new Date(), 'ddmmyyyy_Hms');
+        const filenameToCreate = join(destDir, `movies_${date}.txt`);
         results.forEach(movie => {
-          appendFile(filenameToCreate, movie, (err) => { 
+          const movieLine = `${movie.title} - ${movie.year}\n`;
+          appendFile(filenameToCreate, movieLine, (err) => { 
             if (err) reject();
             else resolve();
           });
